95c11a3d3d33e63af728681a94fbd5dd3cd94746
Alexander McDowell <lextmcdowell@gmail.com>
2022-12-05 21:09:38 -0800

    Day 6

---
+++ 2022/day6/day6.py
@@ -0,0 +1,27 @@
+import time
+
+##########
+# PART 1 #
+##########
+
+start_time = time.time()
+line = open("input.txt", "r+").read().split("\n")[0]
+for i in range(len(line) - 4):
+    substr = line[i : i + 4]
+    if (len(set(substr)) == 4):
+        print(i + 3)
+        break
+print(time.time() - start_time)
+
+##########
+# PART 2 #
+##########
+
+start_time = time.time()
+line = open("input.txt", "r+").read().split("\n")[0]
+for i in range(len(line) - 14):
+    substr = line[i : i + 14]
+    if (len(set(substr)) == 14):
+        print(i + 14)
+        break
+print(time.time() - start_time)
