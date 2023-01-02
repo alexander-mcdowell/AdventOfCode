ebd699c24baeca4716dc8887f36d81748c2a01cf
Alexander McDowell <lextmcdowell@gmail.com>
2023-01-02 17:15:55 -0500

    Day 24 from 2021

--- 2021/day24/day24.py
+++ 2021/day24/day24.py
@@ -2,149 +2,98 @@
 # PART 1 #
 ##########
 
-# All blocks start with:
-#   inp w
-#   mul x 0
-#   add x z
-#   mod x 26
-# Followed by "div z k1" where k1 = 1 or 26
-# Followed by "add x <some number k2>"
-# Then followed by:
-#   eql x w
-#   eql x 0
-#   mul y 0
-#   add y 25
-#   mul y x
-#   add y 1
-#   mul z y
-#   mul y 0
-#   add y w
-# Then followed by "add y <some number k3>"
-# Then concluded with:
-#   mul y x
-#   add z y
+import time
 
-# z[0] = 0
-# z[n] = (z[n - 1] / k1) + (((z[n - 1] % 26) + k2) != d[n]) * (25 * (z[n - 1] / k1) + d[n] + k3)
-# DNE = (((z[n - 1] % 26) + k2) != d[n])
-# z[n] = (z[n - 1] / k1) + DNE * (25 * (z[n - 1] / k1) + d[n] + k3)
+# inp w
+# mul x 0   w=w, x=0, y=0, z=z
+# add x z   w=w, x=z, y=0, z=z
+# mod x 26  w=w, x=(z%26), y=0, z=z
+# div z 1   w=w, x=(z%26), y=0, z=z                               (either 1 or 26)
+# add x 15  w=w, x=(z%26)+15, y=0, z=z                            (arbitrary constant)
+# eql x w   w=w, x=((z%26)+15)==w, y=0, z=z
+# eql x 0   w=w, x=(((z%26)+15)==w)==0, y=0, z=z
+# mul y 0   w=w, x=(((z%26)+15)==w)==0, y=0, z=z
+# add y 25  w=w, x=(((z%26)+15)==w)==0, y=25, z=z
+# mul y x   w=w, x=(((z%26)+15)==w)==0, y=25x, z=z
+# add y 1   w=w, x=(((z%26)+15)==w)==0, y=25x+1, z=z
+# mul z y   w=w, x=(((z%26)+15)==w)==0, y=25x+1, z=z*(25x+1)
+# mul y 0   w=w, x=(((z%26)+15)==w)==0, y=0, z=z*(25x+1)
+# add y w   w=w, x=(((z%26)+15)==w)==0, y=w, z=z*(25x+1)
+# add y 13  w=w, x=(((z%26)+15)==w)==0, y=w+13, z=z*(25x+1)       (arbitrary constant)
+# mul y x   w=w, x=(((z%26)+15)==w)==0, y=x(w+13), z=z*(25x+1)
+# add z y   w=w, x=(((z%26)+15)==w)==0, y=x(w+13), z=z*(25x+1)+x(w+13)
 
-# If DNE = 0:
-# d[n] = (z[n - 1] % 26) + k2
-# z[n] = (z[n - 1] / k1)
-# If DNE = 1:
-# d[n] != (z[n - 1] % 26) + k2
-# z[n] = 26 * (z[n - 1] / k1) + d[n] + k3
+# x = 0: z = z/1 or z/26
+# x = 1: z = (z/1 or z/26)*26 + w + <constant>
 
-"""
-1:
-div z 1
-add x 15
-add y 13
+# start_time = time.time()
+# lines = open("day24in.txt").read().split("\n")
 
-2:
-div z 1
-add x 10
-add y 16
+# a_list = [15, 10, 12, 10, 14, -11, 10, -16, -9, 11, -8, -8, -10, -9]
+# b_list = [13, 16, 2, 8, 11, 6, 12, 2, 2, 15, 1, 10, 14, 10]
+# z_divide = [1, 1, 1, 1, 1, 26, 1, 26, 26, 1, 26, 26, 26, 26]
 
-3:
-div z 1
-add x 12
-add y 2
+# def find(i, z, a_list, b_list, z_divide):
+#       val = z % 26 + a_list[i]
+#       if (z_divide[i] == 26):
+#             # x = 0:
+#             if (not (1 <= val <= 9)): return None
+#             w = val
+#             z //= 26
+#             if (i == 13):
+#                   if (z == 0): return [str(w)]
+#                   else: return None
+#             else:
+#                   temp = find(i + 1, z, a_list, b_list, z_divide)
+#                   if (temp == None): return None
+#                   return [str(w)] + temp
+#       else:
+#             # x = 1:
+#             for w in range(9, 0, -1):
+#                   if (val == w): continue
+#                   z_next = 26 * z + w + b_list[i]
+#                   temp = find(i + 1, z_next, a_list, b_list, z_divide)
+#                   if (temp == None): continue
+#                   return [str(w)] + temp
+#             return None
 
-4:
-div z 1
-add x 10
-add y 8
+# print("".join(find(0, 0, a_list, b_list, z_divide)))
+# print(time.time() - start_time)
 
-5:
-div z 1
-add x 14
-add y 11
+##########
+# PART 2 #
+##########
 
-6:
-div z 26
-add x -11
-add y 6
+start_time = time.time()
+lines = open("day24in.txt").read().split("\n")
 
-7:
-div z 1
-add x 10
-add y 12
+a_list = [15, 10, 12, 10, 14, -11, 10, -16, -9, 11, -8, -8, -10, -9]
+b_list = [13, 16, 2, 8, 11, 6, 12, 2, 2, 15, 1, 10, 14, 10]
+z_divide = [1, 1, 1, 1, 1, 26, 1, 26, 26, 1, 26, 26, 26, 26]
 
-8:
-div z 26
-add x -16
-add y 2
+def find(i, z, a_list, b_list, z_divide):
+      val = z % 26 + a_list[i]
+      if (z_divide[i] == 26):
+            # x = 0:
+            if (not (1 <= val <= 9)): return None
+            w = val
+            z //= 26
+            if (i == 13):
+                  if (z == 0): return [str(w)]
+                  else: return None
+            else:
+                  temp = find(i + 1, z, a_list, b_list, z_divide)
+                  if (temp == None): return None
+                  return [str(w)] + temp
+      else:
+            # x = 1:
+            for w in range(1, 10):
+                  if (val == w): continue
+                  z_next = 26 * z + w + b_list[i]
+                  temp = find(i + 1, z_next, a_list, b_list, z_divide)
+                  if (temp == None): continue
+                  return [str(w)] + temp
+            return None
 
-9:
-div z 26
-add x -9
-add y 2
-
-10:
-div z 1
-add x 11
-add y 15
-
-11:
-div z 26
-add x -8
-add y 1
-
-12:
-div z 26
-add x -8
-add y 10
-
-13:
-div z 26
-add x -10
-add y 14
-
-14:
-div z 26
-add x -9
-add y 10
-"""
-
-ks = [(1, 15, 13),
-      (1, 10, 16),
-      (1, 12, 2),
-      (1, 10, 8),
-      (1, 14, 11),
-      (26, -11, 6),
-      (1, 10, 12),
-      (26, -16, 2),
-      (26, -9, 2),
-      (1, 11, 15),
-      (26, -8, 1),
-      (26, -8, 10),
-      (26, -10, 14),
-      (26, -9, 10)]
-
-# If DNE = 0:
-# d[n] = (z[n - 1] % 26) + k2
-# z[n] = (z[n - 1] / k1)
-# If DNE = 1:
-# d[n] != (z[n - 1] % 26) + k2
-# z[n] = 26 * (z[n - 1] / k1) + d[n] + k3
-
-# Applying this procedure starting from z[0] and testing different DNE values yields:
-
-# z[0] = 0
-
-# z[1] = d[1] + 13
-# d[1] != 15
-
-# z[2] = 26 * d[1] + d[2] + 354
-# d[2] != (d[1] + 13) % 26 + 10
-
-# z[3] = 26^2 * d[1] + 26 * d[2] + d[3] + 9206
-# d[3] != (d[2] + 16) % 26 + 12
-
-# z[4] = 26^3 * d[1] + 26^2 * d[2] + 26 * d[3] + d[4] + 239364
-# d[4] != (d[3] + 2) % 26 + 10
-
-# z[5] = 26^4 * d[1] + 26^3 * d[2] + 26^2 * d[3] + 26 * d[4] + d[5] + 6223464
-# d[5] != (d[4] + 8) % 26 + 14
+print("".join(find(0, 0, a_list, b_list, z_divide)))
+print(time.time() - start_time)
