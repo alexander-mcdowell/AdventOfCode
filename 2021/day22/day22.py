eaaa4ad2934372e825d8dfa18edd7d6ba9b29ee1
Alexander McDowell <lextmcdowell@gmail.com>
2023-01-02 18:37:23 -0500

    Day 22 from 2021

--- 2021/day22/day22.py
+++ 2021/day22/day22.py
@@ -33,18 +33,94 @@ print(total_on)
 # PART 2 #
 ##########
 
+import time
+
+start_time = time.time()
 data = open("day22in.txt").read().split("\n")
 
-total_on = 0
-cuboids = []
+def intersection(cuboid1, cuboid2):
+    intersect = []
+    for i in range(3):
+        # No intersection
+        if (cuboid1[i][0] > cuboid2[i][1] or cuboid1[i][1] < cuboid2[i][0]): return None
+        intersect.append((max(cuboid1[i][0], cuboid2[i][0]), min(cuboid1[i][1], cuboid2[i][1])))
+    return intersect
+
+def exclude(cuboid, exclusion):
+    # Assume that the entire region to exlcude lies within cuboid
+    includes_across_dim = []
+    for i in range(3):
+        include = []
+        if (cuboid[i][0] < exclusion[i][0]): include.append((cuboid[i][0], exclusion[i][0] - 1))
+        if (cuboid[i][1] > exclusion[i][1]): include.append((exclusion[i][1] + 1, cuboid[i][1]))
+        includes_across_dim.append(include + [exclusion[i]])
+
+    included = []
+    for x in includes_across_dim[0]:
+        for y in includes_across_dim[1]:
+            for z in includes_across_dim[2]:
+                if (x != exclusion[0] or y != exclusion[1] or z != exclusion[2]): included.append([x, y, z])
+    return included
+
+def volume(cuboid):
+    v = 1
+    for i in range(3): v *= (cuboid[i][1] - cuboid[i][0]) + 1
+    return v
+
+disjoint_on = []
 for inst in data:
     inst_type, inst = inst.split(" ")
     x_range, y_range, z_range = inst.split(",")
     xlow, xhigh = [int(x) for x in x_range.replace("x=", "").split("..")]
     ylow, yhigh = [int(y) for y in y_range.replace("y=", "").split("..")]
     zlow, zhigh = [int(z) for z in z_range.replace("z=", "").split("..")]
-    
-    xside, yside, zside = xhigh - xlow + 1, yhigh - ylow + 1, zhigh - zlow + 1
-    corner = (xlow, ylow, zlow)
+    cuboid = [(xlow, xhigh), (ylow, yhigh), (zlow, zhigh)]
 
+    if (inst_type == "on"):
+        on_stack = [cuboid]
+        while len(on_stack) != 0:
+            cuboid = on_stack.pop(0)
+    
+            if (len(disjoint_on) == 0):
+                disjoint_on.append(cuboid)
+                continue
+        
+            # Check if there is any intersection between this "on" cuboid and any other cuboid already on
+            flag = True
+            for cuboid2 in disjoint_on:
+                intersect = intersection(cuboid, cuboid2)
+                if (intersect == None): continue
+
+                # Remove the intersection from this "on" cuboid and try again with each newly formed cuboid
+                flag = False
+                after_exclude = exclude(cuboid, intersect)
+                for cuboid3 in after_exclude: on_stack.insert(0, cuboid3)
+                break
+            if (flag): disjoint_on.append(cuboid)
+    else:
+        disjoint_off = [cuboid]
+        while len(disjoint_off) != 0:
+            off_cuboid = disjoint_off.pop(0)
+            i = 0
+
+            # Check if there is any intersection between this "off" cuboid and any "on" cuboid
+            while i != len(disjoint_on):
+                on_cuboid = disjoint_on[i]
+                intersect = intersection(on_cuboid, off_cuboid)
+                if (intersect != None):
+                    disjoint_on.pop(i)
+                    # Remove the intersection from this "on" cuboid and add all of the newly formed cuboids
+                    after_exclude = exclude(on_cuboid, intersect)
+                    for cuboid in after_exclude: disjoint_on.insert(i, cuboid)
+                    # Remove the intersection from this "off" cuboid and try removal again with each newly formed off cuboid
+                    after_exclude = exclude(off_cuboid, intersect)
+                    for cuboid in after_exclude: disjoint_off.insert(0, cuboid)
+                    break
+                i += 1
+
+total_on = 0
+for cuboid in disjoint_on:
+    #print(cuboid)
+    total_on += volume(cuboid)
 print(total_on)
+print(time.time() - start_time)
