8e8327f6687f2be760fed57f85f58743be923a07
alexander-mcdowell <lextmcdowell@gmail.com>
2025-12-04 21:34:00 -0800

    Day 5

---
+++ 2025/day5/day5.py
@@ -0,0 +1,65 @@
+import time
+
+##########
+# PART 1 #
+##########
+
+# start_time = time.time()
+# f = open("input.txt", 'r')
+# data = f.read().split("\n")
+# ranges = []
+# queries = False
+# fresh = 0
+# for l in data:
+#     if queries:
+#         x = int(l)
+#         for range in ranges:
+#             if range[0] <= x <= range[1]:
+#                 fresh += 1
+#                 break
+#     else:
+#         if l=="":
+#             ranges = sorted(ranges, key=lambda x: x[0])
+#             queries = True
+#         else: ranges.append([int(x) for x in l.split("-")])
+# print(fresh)
+# print(time.time() - start_time)
+
+##########
+# PART 2 #
+##########
+
+start_time = time.time()
+f = open("input.txt", 'r')
+data = f.read().split("\n")
+ranges = []
+queries = False
+fresh = 0
+for l in data:
+    if l=="":
+        break
+    else: ranges.append([int(x) for x in l.split("-")])
+
+fresh = 0
+prev = None
+ranges = sorted(ranges, key=lambda x: x[0])
+for i in range(len(ranges)):
+    interval = ranges[i]
+    # Add first interval
+    if prev==None:
+        fresh += interval[1]-interval[0]+1
+        prev = interval
+    else:
+        # Interval is completely contained in previous, do not add it
+        if prev[0] <= interval[0] <= prev[1] and prev[0] <= interval[1] <= prev[1]:
+            continue
+        # Lower part of interval is contained in previous
+        elif prev[0] <= interval[0] <= prev[1]:
+            fresh += interval[1] - max(interval[0], prev[1]+1) + 1
+        # Disjoint intervals
+        else:
+            fresh += interval[1]-interval[0]+1
+        prev = [prev[0], interval[1]]
+
+print(fresh)
+print(time.time() - start_time)
